55a4f3a9f9aa7db875ab2033c8c02842e8ff9858
voltycodes <153023205+voltycodes@users.noreply.github.com>
2024-10-07 02:45:06 +0530

    fix font issues

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				sans: ['Inconsolata', 'monospace', ...defaultTheme.fontFamily.mono],
-			},
 			colors: {
 				background: '#000',
 				foreground: '#fff',
