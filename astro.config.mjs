18a426a7d7786b1128e067348e3c1b2b1a161cb2
voltycodes <153023205+voltycodes@users.noreply.github.com>
2024-10-18 01:36:43 +0530

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://voltycodes.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'hybrid',
+  adapter: vercel()
 });
