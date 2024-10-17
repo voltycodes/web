c475c0ea56a7d59e1835a84e84b6e270ec043ed3
voltycodes <153023205+voltycodes@users.noreply.github.com>
2024-10-18 01:00:53 +0530

    feat(lanyard): add lanyard api

---
+++ src/pages/w/resources.md
@@ -0,0 +1,7 @@
+---
+layout: ../../layouts/PostLayout.astro
+title: Resources
+description: Curated list of resources that I find useful.
+created: 2024-10-07
+lastUpdated: 2024-10-07
+---
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
+const colors = ['green-400', 'red-400', 'yellow-400', 'gray-400'];
+
+const generateSafelist = (colors) => {
+  return colors.flatMap(color => [
+    `bg-${color}`,
+    `border-${color}`
+  ]);
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist: generateSafelist(colors),
 	theme: {
 		extend: {
 			colors: {
